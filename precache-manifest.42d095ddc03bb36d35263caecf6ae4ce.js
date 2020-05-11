ea25fba8ce8fc6e4c8d089864575608504405bfb
Sachin <sheelavantsachin@gmail.com>
2020-05-12 01:24:35 +0530

    Updates

---
+++ precache-manifest.42d095ddc03bb36d35263caecf6ae4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58bdb76149f3089d4dd9079ee982c73",
+    "url": "/react-currencyconverter/index.html"
+  },
+  {
+    "revision": "21ae2c9aa49f26217c36",
+    "url": "/react-currencyconverter/static/css/main.1fdc06d5.chunk.css"
+  },
+  {
+    "revision": "995d0bb35bcb866996ae",
+    "url": "/react-currencyconverter/static/js/2.d7818a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currencyconverter/static/js/2.d7818a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ae2c9aa49f26217c36",
+    "url": "/react-currencyconverter/static/js/main.ef6a9af7.chunk.js"
+  },
+  {
+    "revision": "4fc0cdeebe7034b19b56",
+    "url": "/react-currencyconverter/static/js/runtime-main.bd331b48.js"
+  }
+]);
